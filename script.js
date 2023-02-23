151e0fdd93b3108fc74da4eeef465ff2d2c19894
Kaylachin90 <123793595+Kaylachin90@users.noreply.github.com>
2023-02-23 10:52:02 -0800

    finished generator

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword (){
+  var passwordLength = parseInt(prompt("Enter your desired password length"));
+  console.log (passwordLength);
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null || isNaN(passwordLength)) {
+    alert("Please enter a length between 8 and 128");
+    return "Please Try Again";
+  }
+  var acceptLower = confirm("Would you like lower case letters?");
+  console.log(acceptLower);
+  var totalString = ""
+  if (acceptLower) {
+    //add lower characters to total
+totalString += "abcde"
+  }
+  if (confirm("Would you like uppercase letters?")) {
+    //add upper characters to toal
+    totalString += "ABCD"
+  }
+  if (confirm("Would you like numbers?")) {
+    //add numbers to toal
+    totalString += "0123456789"
+  }
+  if (confirm("Would you like special characters?")) {
+    //add specials characters to toal
+    totalString += "!@#$%^&*"
+  }
+  console.log(totalString);
+  if (totalString === "") {
+    alert("Must select at least 1 character type")
+    return "Please Try Again";
+  }
+  var totalArray = totalString.split("") 
+  var passwordString = ""
+  for (var i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random()*totalArray.length);
+    var digit = totalArray[index];
+    passwordString += digit
+  }
+  return passwordString
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password)
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+passwordText.value = password
 
 }
 
